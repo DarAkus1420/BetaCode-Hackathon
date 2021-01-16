9263d3619109974f7f3d3b941466fe97a4285658
Angelica Simarra <48442137+asimarra@users.noreply.github.com>
2021-01-16 18:41:09 -0500

    Delete prueba.js

--- api/prueba.js
+++
@@ -1,6 +0,0 @@
-const youtube = require('scrape-youtube').default;
-
-youtube.search('perfect ed-sheeran', { type: 'video' }).then(results => {
-	// Unless you specify a type, it will only return 'video' results
-	console.log(results.videos);
-});
