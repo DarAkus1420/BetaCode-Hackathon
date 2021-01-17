f5580e688a551cf83d898b3335a29b06c72d8b2d
Claudio <claudioan00@gmail.com>
2021-01-17 17:14:26 -0300

    barra busqueda

--- frontend/src/components/Busqueda.js
+++ frontend/src/components/Busqueda.js
@@ -1,7 +1,27 @@
 import React from 'react';
+/*import { useCombobox } from 'downshift';
+import { Input } from 'antd';*/
 
 const Busqueda = () => {
-	return <input type="search" className="search" placeholder="Busqueda"></input>;
+	return (
+		<>
+			<form>
+				<div class="form-group">
+					<label for="busqueda">Buscar canción</label>
+					<input
+						type="text"
+						class="form-control"
+						id="busqueda"
+						aria-describedby="busquedaCancion"
+						placeholder="Ingresa canción"
+					/>
+					<small id="ayudaBusqueda" class="form-text text-muted">
+						Ingresa la canción que quieres buscar
+					</small>
+				</div>
+			</form>
+		</>
+	);
 };
 
 export default Busqueda;
--- frontend/src/components/Home.js
+++ frontend/src/components/Home.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import Esquema from './Esquema';
 import Header from './Header';
+import Busqueda from './Busqueda';
 //import Footer from './Footer';
 
 export default function Home() {
 	return (
 		<div>
 			<Header />
+			<Busqueda />
 			<Esquema />
 		</div>
 	);
